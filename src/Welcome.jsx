4bc79ae605c62a1cba1d95ab16781a31651cfdaa
nitish-002 <surya0424sharma@gmail.com>
2025-03-17 10:56:58 +0530

    decrement in card

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -10,12 +10,19 @@ class Welcome extends Component{
     increment = () => { // no function keyword needed here
         this.setState({count:this.state.count+1})
     }
+    decrement= ()=>{
+        this.setState({count:this.state.count-1})
+    }
+    alertfunction =()=>{
+        alert("Can't go further")
+    }
     render(){
         return (
             <div>
             {/* <h1>Welcome, {this.props.name}</h1> */}
             <h1>{this.state.count}</h1>
-            <button onClick={this.increment}>+</button>
+            <button onClick={this.state.count<10?this.increment:this.alertfunction}>+</button>
+            <button onClick={this.state.count>-10? this.decrement:this.alertfunction}>-</button>
             </div>
         )
     }
